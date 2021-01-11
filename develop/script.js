f42a748fc209af2aa6bb9552e7567598ae7366e4
caroline-e-miller <caroline.miller@ymail.com>
2021-01-11 12:10:19 -0600

    removed unnecessary comments from js and index

--- develop/script.js
+++ develop/script.js
@@ -4,12 +4,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = createPassword();//generatePassword();
+    var password = createPassword();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 }
-// declare variables
+// declare prompt variables
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "q", "x", "y", "z"];
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -17,8 +17,6 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-
-// create onlick event that triggers prompts(?)
 
 function createPassword() {
 
@@ -37,21 +35,21 @@ function createPassword() {
         alert("Your password must be between 8 and 128 characters.");
     } else if (!upper && !lower && !special && !num) {
         alert("You need to select at least one criteria to generate a password.")
+        // if user inputs text, an alert appears reminding them to use numbers
+    } else if (isNaN(length)) {
+        alert("You must input a number for the length of your password.")
+
     } else {
         let randomIndex = 0;
         if (upper) {
-            // upperCase[Math.floor(Math.random() * upperCase.length)]
             characterOptions = characterOptions.concat(upperCase);
         }
         if (lower) {
             characterOptions = characterOptions.concat(lowerCase);
-            // lowerCase[Math.floor(Math.random() * lowerCase.length)]
         } if (special) {
             characterOptions = characterOptions.concat(specialChars);
-            // specialChars[Math.floor(Math.random() * specialChars.length)]
         } if (num) {
             characterOptions = characterOptions.concat(numbers);
-            // numbers[Math.floor(Math.random() * numbers.length)]
         }
         while (generatedPassword.length < length) {
             randomIndex = Math.floor(Math.random() * characterOptions.length)
@@ -59,14 +57,8 @@ function createPassword() {
         }
     }
 
-    // Ask user for specifications regarding uppercase, lowercase, numbers, and special characters. If none are selected, display alert
-
-
     return generatedPassword;
 }
-
-//createPassword();
-
 
 // Add event listener to generate button (when user presses button, the password is written)
 generateBtn.addEventListener("click", writePassword);
